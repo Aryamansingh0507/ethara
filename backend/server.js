e88a5805d08c306ecb9ba67d92e2cd167e12ba73
Aryamansingh0507 <aryamansingh902@gmail.com>
2026-05-06 22:37:03 +0530

    updated

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,32 @@
 const express = require("express");
-const mongoose = require("mongoose");
 const cors = require("cors");
 const dotenv = require("dotenv");
 
 dotenv.config();
 
-const app = express();
-
-app.use(cors());
-app.use(express.json());
+const connectDB = require("./config/database");
 
 const authRoutes = require("./routes/authRoutes");
 const projectRoutes = require("./routes/projectRoutes");
 const taskRoutes = require("./routes/taskRoutes");
 
+const app = express();
+
+// Connect Database
+connectDB();
+
+// Middleware
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://yourfrontend.up.railway.app"
+  ],
+  credentials: true
+}));
+
+app.use(express.json());
+
+// Routes
 app.get("/", (req, res) => {
   res.send("API Running");
 });
@@ -22,12 +35,9 @@ app.use("/api/auth", authRoutes);
 app.use("/api/projects", projectRoutes);
 app.use("/api/tasks", taskRoutes);
 
-mongoose.connect(process.env.MONGO_URI)
-.then(() => {
-  console.log("MongoDB Connected");
+// Port
+const PORT = process.env.PORT || 5000;
 
-  app.listen(5000, () => {
-    console.log("Server running on port 5000");
-  });
-})
-.catch((err) => console.log(err));
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
